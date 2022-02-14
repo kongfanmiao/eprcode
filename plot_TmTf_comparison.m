d86a51a8b07d8e7bba4a9ca2e1006daa940ba3c5
Fanmiao Kong <fanmiaokong1223@gmail.com>
2022-02-14 15:05:33 +0000

    Update plot_TmTf_comparison.m

--- plot_TmTf_comparison.m
+++ plot_TmTf_comparison.m
@@ -7,8 +7,10 @@ hold on
 scatter(temperatue, Tf, 100, 'o');
 ylabel("T_m & T_f (ns)")
 yyaxis right
-scatter(temperatue, TmCoef*100, 100, 'filled', 'd');
-scatter(temperatue, TfCoef*100, 100, 'd');
+TmPerc = abs(TmCoef)./(abs(TmCoef)+abs(TfCoef));
+TfPerc = abs(TfCoef)./(abs(TmCoef)+abs(TfCoef));
+scatter(temperatue, TmPerc*100, 100, 'filled', 'd');
+scatter(temperatue, TfPerc*100, 100, 'd');
 ylabel('Percentage (%)')
 legend('T_m', 'T_f', 'T_m percentage', 'T_f percentage', "Location","best");
 hold off
