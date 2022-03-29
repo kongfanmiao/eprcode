3addb68bae4d2964a4f12f0baf56232077fec829
Fanmiao Kong <fanmiaokong1223@gmail.com>
2022-03-29 14:50:51 +0100

    Change the way to input keywords

--- find_files.m
+++ find_files.m
@@ -1,30 +1,28 @@
 function outputFiles = find_files(path, keywords)
 % Find files in given path based on give keywords. The keywords can be as 
-% many as you want, and can be in any sequence
+% many as you want, and can be in any sequence. They keywords list should be
+% a string array.
 arguments
     path string
-end
-arguments (Repeating)
-    keywords string
+    keywords string % string array
 end
 
-keywordsList = horzcat(keywords{:}); % string array
 % check what file type to read
-if ~any(contains(keywordsList, "DTA") | contains(keywordsList, "DSC"))
-    keywordsList(end+1) = "DTA"; % by default read DTA file, if no file 
+if ~any(contains(keywords, "DTA") | contains(keywords, "DSC"))
+    keywords(end+1) = "DTA"; % by default read DTA file, if no file 
                                   % type is provided
 end
 
 % if the temperature keywords is provides, prepend a '_' before searching
 % the file
-idxK = contains(keywordsList, "K");
+idxK = contains(keywords, "K");
 if any(idxK) % if constains a temperature string
-    keywordsList(idxK) = strcat("_",keywordsList(idxK));
+    keywords(idxK) = strcat("_",keywords(idxK));
 end
 
 % Now search the files that contain all of the keywords
 allFiles = {dir(path).name}';
-indices = cellfun(@(s)contains(allFiles,s), keywordsList, ...
+indices = cellfun(@(s)contains(allFiles,s), keywords, ...
     "UniformOutput",false);
 finIdx = all(horzcat(indices{:}),2);
 outputFiles = allFiles(finIdx); % one column cell array
