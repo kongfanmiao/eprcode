c232d095089bfb304507dfa1a83a43d61283e226
Fanmiao Kong <fanmiaokong1223@gmail.com>
2022-04-11 18:06:43 +0100

    Update fit_Tm.m
    Similar changes as fit_T1

--- fit_Tm.m
+++ fit_Tm.m
@@ -1,117 +1,173 @@
-function [FitResult,X,Yfit] = fit_Tm(path, keywords, fitfunc, MarkerSize)
-% Plot data files in given path based on given keywords. The keywords can 
-% be as many as you want, and can be in any sequence.
+% fit_Tm    Fit the Tm measurement data
+%
+%   FitResult = fit_Tm(path, keywords)
+%   FitResult = fit_Tm(path, keywords, FitFunc)
+%   FitResult = fit_Tm(path, keywords, 'MarkerSize', 20)
+%   FitResult = fit_Tm(path, keywords, 'TimeUnit', 'ns')
+%
+%   Input:
+%       path        directory to find files
+%       keywords    keywords based on which to find files
+%                   - string: "80K" (if there's only one keyword)
+%                   - character vector: '80K' (if there's only one keyword)
+%                   - string array: ["80K", "3480G"]
+%                   - cell array of character vectors: {'80K', '3480G'}
+%       FitFunc     fit function, usually use bi-exponential  
+%                   - {'Exp1', 'Exp2', 'StrExp'}
+%       MarkerSize  marker size in scatter plot, 20 by default
+%                   - Name-value pair arguments
+%       TimeUnit    X axis unit, use microsecond (us) by default 
+%                   - {'ns', 'us', 'ms', 's'}
+%
+%   Output:
+%       FitResult   table containing fitting results.
+%
+%   Example:
+%       Res = fit_T1('./Data',["3480G","5K"],'StrExp','TimeUnit','ns')
+%
 
-arguments
-    path string
-    keywords string
-    fitfunc string {mustBeMember(fitfunc,{'Exp1','Exp2','StrExp'})}= 'Exp2'
-    MarkerSize double = 20
-end
+
+function FitResult = fit_Tm(path, keywords, varargin)
+
+% parse the input arguments
+par = inputParser;
+
+checkFitFunc = @(x) any(validatestring(x, {'Exp1', 'Exp2', 'StrExp'}));
+par.addOptional('FitFunc', 'Exp2', checkFitFunc);
+
+par.addParameter('MarkerSize', 20, @isnumeric);
+
+checkTimeUnit = @(x) any(validatestring(x, {'ns', 'us', 'ms', 's'}));
+par.addParameter('TimeUnit', 'ns', checkTimeUnit);
+
+par.KeepUnmatched = true;
+
+parse(par, varargin{:});
+args = par.Results;
+fitfunc = args.FitFunc;
 
 fitfuncFullName = containers.Map( ...
     ["Exp1", "Exp2", "StrExp"], ...
     ["Mono-exponential", "Bi-exponential", "Stretched Exponential"]);
 
 % Get the list of file names
+% Convert to string if it's cell array of character vectors
+keywords = string(keywords); 
 keywords(end+1) = "Tm";
 keywords = unique(keywords);
-files = find_files(path, keywords(:));
-% Extract the temperature string according to given pattern
-tempStr = cellfun(@(s)regexp(s,"\d*\.*\d*K", "match"), files);
-% Convert to double data type
-temperature = cellfun(@(s)str2double(s(1:end-1)),tempStr);
-% Sort the files according to temperature in ascending order
-filesTable = table(files, temperature);
-filesTable = sortrows(filesTable, "temperature");
-temperature = filesTable.temperature;
-files = filesTable.files;
-% Convert cell array to matrix
-keywordsList = horzcat(keywords(:)); % string array
+filesTable = sort_temperature(path, keywords);
+Temperature = filesTable.Temperature;
+Files = filesTable.Files;
+numFiles = length(Files);
+
 % Create the figure title based on keywords
-titleStr = join(keywordsList, " ");
+titleStr = join(keywords, " ");
 % Create figure legends
-labels = cell(2*length(files),1);
+labels = cell(2*numFiles,1);
+
+timeUnit = args.TimeUnit;
+switch timeUnit
+    case 'ns'
+        timeScaleFactor = 1;
+    case 'us'
+        timeScaleFactor = 1e3;
+        timeUnit = '\mus';
+    case 'ms'
+        timeScaleFactor = 1e6;
+    case 's'
+        timeScaleFactor = 1e9;
+end
 
 % Set the x and y axis label
-xlabelStr = strcat("2",char([0xD835 0xDF0F]), " (ns)");
+xlabelStr = sprintf(strcat("2",char([0xD835 0xDF0F]), " (%s)"), timeUnit);
 ylabelStr = "Signal";
 
 xMax = 0;
 xMin = inf;
 yMax = 0;
 yMin = inf;
-colorList = jet(length(files));
+colorList = jet(numFiles);
+
+% Initialize FitResult table
 switch fitfunc
     case "Exp1"
-        func = "c1 + c2*exp(-k*x)";
-        num_k = 1; num_c = 2;
+        FitResult = table('VariableNames',{'Temperature', 'Tm'}, ...
+            'Size', [numFiles, 2], 'VariableTypes',repelem({'double'},2));
+        FitResult.Properties.VariableUnits = {'K', timeUnit};
     case "Exp2"
-        func = "c1 + c2*exp(-k1*x) + c3*exp(-k2*x)";
-        num_k = 2; num_c = 3;
+        FitResult = table('VariableNames',{'Temperature', ...
+            'Tm', 'Tm Percentage', 'Tf', 'Tf Percentage'}, ...
+            'Size', [numFiles, 5], 'VariableTypes',repelem({'double'},5));
+        FitResult.Properties.VariableUnits = {'K', timeUnit, '', ...
+            timeUnit, ''};
     case "StrExp"
-        func = "c1 + c2*exp(-(k*x)^c3)";
-        num_k = 1; num_c = 3;
+        FitResult = table('VariableNames',{'Temperature', ...
+            'Tm', 'Stretch Factor'}, ...
+            'Size', [numFiles, 3], 'VariableTypes',repelem({'double'},3));
+        FitResult.Properties.VariableUnits = {'K', timeUnit, ''};
 end
-X = [];
-Yfit = [];
-k = zeros(length(files),num_k);
-c = zeros(length(files),num_c);
-rmse = zeros(size(files)); % Root mean squared error
-FitResult = table(temperature, c, k, rmse);
-for i = 1:length(files)
-    f = files{i};
+
+FitResult.Temperature = Temperature;
+FitResult.("R-Squared") = zeros(numFiles,1);
+
+% Do the fitting
+for i = 1:numFiles
+    f = Files{i};
     [x, y] = eprload(fullfile(path, f));
-    y = real(y);
-    y = y/max(y); % normalize
-    % fit
+    y = real(y);          
     % first use exponfit to get an estimate
+    % "c1 + c2*exp(-k*x)"
     [k,c,yfit] = exponfit(x,y,1);
     switch fitfunc
         case "Exp1"
-            fo = fitoptions(func,"StartPoint",[c k]);
+            func = "c1 + c2*exp(-x/k)";
+            fo = fitoptions(func,"StartPoint",[c 1/k]);
             ft = fittype(func,"options",fo);
             [curve, gof] = fit(x,y,ft);
-            FitResult.c(i,:) = [curve.c1 curve.c2];
-            FitResult.k(i,:) = curve.k;
-            FitResult.rmse(i) = gof.rmse; 
+            FitResult.Tm(i) = curve.k;
+            FitResult.("R-Square")(i) = gof.rsquare;
             yfit = curve(x);
         case "Exp2"
-            bounds = [c/100 c(2)/100 k/100 k/100;...
-                      c*100 c(2)*100 k*100 k*100];
+            func = "c1 + c2*exp(-x/k1) + c3*exp(-x/k2)";
+            bounds = [c(1)/2 c(2)/100 c(2)/100 0.001/k 0.001/k;...
+                      c(1)*2 c(2)*100 c(2)*100 1000/k 1000/k];
             fo = fitoptions(func, ...
-                "StartPoint",[c c(2) k k], ...
+                "StartPoint",[c(1) c(2) c(2) 1/k 1/k], ...
                 "Lower",min(bounds,[],1), ...
                 "Upper",max(bounds,[],1));
             ft = fittype(func,"options",fo);
             [curve, gof] = fit(x,y,ft);
-            FitResult.c(i,:) = [curve.c1 curve.c2 curve.c3];
-            FitResult.k(i,:) = [curve.k1 curve.k2];
-            FitResult.rmse(i) = gof.rmse; 
+            [Tm, TmPerc, Tf, TfPerc] = get_TandTf( ...
+                [curve.k1, curve.k2], [curve.c1, curve.c2, curve.c3], ...
+                timeScaleFactor);
+            FitResult{i,2:end-1} = [Tm, TmPerc, Tf, TfPerc];
+            FitResult.("R-Squared")(i) = gof.rsquare; 
             yfit = curve(x);
         case "StrExp"
-            bounds = [c/100 0.1 k/100;...
-                      c*100 1.2 k*100];
+            func = "c1 + c2*exp(-(x/k)^c3)";
+            bounds = [c/100 0.2 0.01/k;...
+                      c*100 2.0 100/k];
             fo = fitoptions(func, ...
-                "StartPoint",[c 1 k], ...
+                "StartPoint",[c 1 1/k], ...
                 "Lower",min(bounds,[],1), ...
                 "Upper",max(bounds,[],1));
             ft = fittype(func,"options",fo);
             [curve, gof] = fit(x,y,ft);
-            FitResult.c(i,:) = [curve.c1 curve.c2 curve.c3];
-            FitResult.k(i,:) = curve.k;
-            FitResult.rmse(i) = gof.rmse; 
+            FitResult{i,2:end-1} = [curve.k, curve.c3];
+            FitResult.("R-Squared")(i) = gof.rsquare; 
             yfit = curve(x);
     end
-%     x = reshape(x,1,[]);
-%     yfit = reshape(yfit,1,[]);
-%     X(end+1,:) = x;
-%     Yfit(end+1,:) = yfit;
-    scatter(x,y,MarkerSize, "MarkerEdgeColor",colorList(i,:));
-    labels{2*i-1} = strcat(num2str(temperature(i))," K data");
+    % Change x axis unit
+    x = x/timeScaleFactor;
+    % Normalize y axis
+    yM = max(y);
+    y = y/yM;
+    yfit = yfit/yM;
+    scatter(x,y,args.MarkerSize, "MarkerEdgeColor",colorList(i,:));
+    labels{2*i-1} = strcat(num2str(Temperature(i))," K data");
     hold on
     plot(x,yfit, "Color", colorList(i,:));
-    labels{2*i} = strcat(num2str(temperature(i))," K Fit");
+    labels{2*i} = strcat(num2str(Temperature(i))," K Fit");
     hold on
     xMin = min(xMin, min(x));
     xMax = max(xMax, max(x));
@@ -120,19 +176,20 @@ for i = 1:length(files)
 end
 title(strcat(titleStr, sprintf("\n(%s)",fitfuncFullName(fitfunc))), ...
     "Interpreter","none");
-legend(labels, "Location","northeastoutside", "Interpreter","none");
+legend(labels, "Location","best", "Interpreter","none");
 ylabel(ylabelStr);
 xlabel(xlabelStr);
 xlim([xMin xMax]);
 ylim([yMin yMax]);
 yticks([]);
 hold off
+set(gcf,'color','w');
+box on
 
 fig = gcf;
-if ~(fig.WindowStyle == "docked")
+if fig.WindowStyle ~= "docked"
     set(fig,'position',[10,10,900,600]);
 end
 
 end
-
 
