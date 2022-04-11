a3c0efa5765afc4265b6999bcf158dd4a07f15af
Fanmiao Kong <fanmiaokong1223@gmail.com>
2022-04-11 18:10:12 +0100

    Create plot_TmTf.m

---
+++ plot_TmTf.m
@@ -0,0 +1,8 @@
+function plot_TmTf(FitResult)
+
+% Plot the Tm, Tf and their percentages for varying temperatures
+% FitResult: output of fit_Tm with the bi-exponential fitting model
+
+plot_TandTf(FitResult, 'Tm')
+
+end
