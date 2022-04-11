21b9bd54a07b8d5ad185c60ffa375129ec861e20
Fanmiao Kong <fanmiaokong1223@gmail.com>
2022-04-11 18:05:00 +0100

    Update find_files.m
    keywords are not character vectors.
    The output can be full file path

--- find_files.m
+++ find_files.m
@@ -1,10 +1,11 @@
-function outputFiles = find_files(path, keywords)
+function outputFiles = find_files(path, keywords, fullPathOutput)
 % Find files in given path based on give keywords. The keywords can be as 
 % many as you want, and can be in any sequence. They keywords list should be
 % a string array.
 arguments
-    path string
+    path char
     keywords string % string array
+    fullPathOutput logical = false
 end
 
 % check what file type to read
@@ -12,6 +13,7 @@ if ~any(contains(keywords, "DTA") | contains(keywords, "DSC"))
     keywords(end+1) = "DTA"; % by default read DTA file, if no file 
                                   % type is provided
 end
+
 
 % if the temperature keywords is provides, prepend a '_' before searching
 % the file
@@ -26,6 +28,11 @@ indices = cellfun(@(s)contains(allFiles,s), keywords, ...
     "UniformOutput",false);
 finIdx = all(horzcat(indices{:}),2);
 outputFiles = allFiles(finIdx); % one column cell array
+
+if fullPathOutput
+    outputFiles = cellfun(@(file)fullfile(path,file), outputFiles, ...
+        'UniformOutput',false);
+end
 
 % call error if no files are found
 if isempty(outputFiles)
