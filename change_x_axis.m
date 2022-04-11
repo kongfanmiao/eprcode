bdf0fec76f2e092fae0c7c8dab14d70d58e27340
Fanmiao Kong <fanmiaokong1223@gmail.com>
2022-04-11 18:03:32 +0100

    Update change_x_axis.m
    Now accept DTA file. If so it will automatically find the corresponding DSC file

--- change_x_axis.m
+++ change_x_axis.m
@@ -1,15 +1,26 @@
-function xAxis = change_x_axis(N, DSCfilePath)
+function xNew = change_x_axis(xOld, DSCfilePath)
 
 % Change the x axis of T1 and Tm measurement
-% xAxis: one-dimensional data array
-% DSCfilePath: absolute path of the DSC file
+% xNew: one-dimensional data array
+% DSCfilePath: path of the DSC file
+
+arguments
+    xOld double
+    DSCfilePath char
+end
+
+N = numel(xOld);
+if endsWith(DSCfilePath, '.DTA')
+    DSCfilePath = strcat(DSCfilePath(1:end-3),'DSC');
+end
+
 p0 = find_parameter(DSCfilePath, "p0");
 p2 = find_parameter(DSCfilePath, "p2");
 d2 = find_parameter(DSCfilePath, "d2");
 d3 = find_parameter(DSCfilePath, "d3");
 d30 = find_parameter(DSCfilePath, "d30");
-% the i-th value of xAxis = d3+d30*(1+2+...+i-1)+0.5*p9-p2
-xAxis = d3+d30*[1:N]'.*[0:N-1]'/2+0.5*p0-p2;
+% the i-th value of xNew = d3+d30*(1+2+...+i-1)+0.5*p9-p2
+xNew = d3+d30*(1:N)'.*(0:N-1)'/2+0.5*p0-p2;
 end
 
 function output = find_parameter(DSCfilePath, param)
