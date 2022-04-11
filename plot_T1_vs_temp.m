62fa662aebd7f4654fdb12ef5a2ae38e358def82
Fanmiao Kong <fanmiaokong1223@gmail.com>
2022-04-11 18:09:21 +0100

    Update plot_T1_vs_temp.m
    Rewrite arguments parsing

--- plot_T1_vs_temp.m
+++ plot_T1_vs_temp.m
@@ -1,32 +1,55 @@
-function plot_T1_vs_temp(temp,T1,Color,Marker,MarkerSize,inverseT1)
+% plot_T1_vs_temp   plot T1 value against temperature
+%
+%   plot_T1_vs_temp(FitResult)
+%   plot_T1_vs_temp(FitResult,'inverseT1')
+%   plot_T1_vs_temp(FitResult,'Color','k','Marker','*','MarkerSize',50)
+%
+%   Input:
+%       FitResult       table of fitting result
+%       inverseT1       plot the inverse of T1 values
+%       Color           color, 'blue' by default
+%       Marker          marker, 'o' by default
+%       MarkerSize      marker size, 100 by default
+%
+%       
+function plot_T1_vs_temp(FitResult,varargin)
 
-arguments
-    temp double
-    T1 double
-    Color = 'm'
-    Marker {mustBeMember(Marker,{'o','+','*','.','x','-','|','s','d',...
-        '^','v','>','<','p','h'})} = 'p'
-    MarkerSize double = 100
-    inverseT1 logical = false
-end
+par = inputParser;
+par.addOptional('inverseT1', '', @(x)any(validatestring(x, ...
+    {'', 'inverse'})));
+par.addParameter('Color', 'b', @(x)isstring(x)|ischar(x));
+par.addParameter('Marker', 'o', @(x)any(validatestring(x, ...
+    {'o','+','*','.','x','-','|','s','d','^','v','>','<','p','h'})));
+par.addParameter('MarkerSize', 100, @isnumeric);
+par.KeepUnmatched = true;
 
-if inverseT1
+parse(par, varargin{:});
+args = par.Results;
+
+temp = FitResult.Temperature;
+T1 = FitResult.T1;
+
+if ~isempty(args.inverseT1)
     y = 1./T1;
-    ylab = "1/T_1 (1/\mus)";
+    ylab = sprintf("1/T_1 (1/%s)", ...
+        FitResult.Properties.VariableUnits{2});
     ttl = "1/T_1 versus Temperature";
 else
     y = T1;
-    ylab = "T_1 (\mus)";
+    ylab = sprintf("T_1 (%s)", ...
+        FitResult.Properties.VariableUnits{2});
     ttl = "T_1 versus Temperature";
 end
 
-scatter(temp, y, MarkerSize,Marker,Color,'filled')
+scatter(temp, y, args.MarkerSize, args.Marker, args.Color,'filled')
 xlabel("Temperature (K)");
 ylabel(ylab);
 title(ttl)
+set(gcf,'color','w');
+box on
 
 fig = gcf;
-if ~(fig.WindowStyle == "docked")
+if fig.WindowStyle ~= "docked"
     set(fig,'position',[10,10,900,600]);
 end
 
