e23698c5a821343f0c23361344fb344b1afe9b1a
Fanmiao Kong <fanmiaokong1223@gmail.com>
2022-04-11 18:10:08 +0100

    Create plot_TmStretch.m

---
+++ plot_TmStretch.m
@@ -0,0 +1,28 @@
+function plot_TmStretch(FitResult)
+
+% Plot the Tm, and stretch factor for varying temperatures
+% FitResult: output of fit_Tm with the stretched exponential fitting model
+
+xlabel('Temperature (K)');
+
+yyaxis left
+scatter(FitResult.Temperature, FitResult.Tm, 100, 'filled','o');
+ylabel(sprintf("T_m (%s)", FitResult.Properties.VariableUnits{2}));
+
+yyaxis right
+sf = FitResult.('Stretch Factor');
+scatter(FitResult.Temperature, sf, 100, '*');
+ylabel('Stretch Factor')
+ylim([2*min(sf)-max(sf), 2*max(sf)-min(sf)]);
+
+legend('T_m', 'Stretch Factor', "Location","best");
+hold off
+set(gcf,'color','w');
+box on
+
+fig = gcf;
+if fig.WindowStyle ~= "docked"
+    set(fig,'position',[10,10,900,600]);
+end
+
+end
