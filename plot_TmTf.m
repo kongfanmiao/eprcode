038467b96270ba27d309b3c996ea06b724bb2abc
Fanmiao Kong <fanmiaokong1223@gmail.com>
2022-04-24 16:03:31 +0100

    Delete plot_TmTf.m

--- plot_TmTf.m
+++
@@ -1,8 +0,0 @@
-function plot_TmTf(FitResult)
-
-% Plot the Tm, Tf and their percentages for varying temperatures
-% FitResult: output of fit_Tm with the bi-exponential fitting model
-
-plot_TandTf(FitResult, 'Tm')
-
-end
