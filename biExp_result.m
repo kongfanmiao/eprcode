b470a4241d253260dfb46380dcc2c09173a8854c
Fanmiao Kong <fanmiaokong1223@gmail.com>
2022-04-24 16:01:15 +0100

    Create biExp_result.m

---
+++ biExp_result.m
@@ -0,0 +1,46 @@
+function biExp_result(FitResult, plot_percentage)
+
+% Plot the T_long, T_short and their percentages for varying temperatures
+% FitResult: output of fit_T1(fit_Tm) with the bi-exponential fitting model
+
+arguments
+    FitResult   table
+    plot_percentage logical = true
+end
+
+
+if plot_percentage
+    yyaxis left
+end
+
+scatter(FitResult.Temperature, FitResult.T_long, 100, 'blue', 'filled','o');
+hold on
+scatter(FitResult.Temperature, FitResult.T_short, 100,'blue', 'o');
+ylabel(sprintf("T_{long} & T_{short} (%s)", ...
+    FitResult.Properties.VariableUnits{2}));
+
+legend('T_{long}', 'T_{short}', "Location","best");
+
+if plot_percentage
+    yyaxis right
+    scatter(FitResult.Temperature, ...
+        FitResult.('T_long Percentage'), 100, 'filled', '^');
+    scatter(FitResult.Temperature, ...
+        FitResult.('T_short Percentage'), 100, 'v');
+    ylabel('Percentage')
+    
+    legend('T_{long}', 'T_{short}', 'T_{long} percentage', ...
+        'T_{short} percentage', "Location","best");
+end
+
+xlabel('Temperature (K)');
+hold off
+set(gcf,'color','w');
+box on
+
+fig = gcf;
+if fig.WindowStyle ~= "docked"
+    set(fig,'position',[10,10,900,600]);
+end
+
+end
