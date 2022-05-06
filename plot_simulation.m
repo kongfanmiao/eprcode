82dc634999e5b19a37996cc09fee4da47998e7f6
Fanmiao Kong <fanmiaokong1223@gmail.com>
2022-05-06 11:43:10 +0100

    Create plot_simulation.m

---
+++ plot_simulation.m
@@ -0,0 +1,33 @@
+function plot_simulation(path, fileName)
+
+filePath = fullfile(path, fileName);
+
+data = readtable(filePath, 'Range','A3');
+names = readcell(filePath, 'Range', 'A1:C1');
+units = readcell(filePath, 'Range', 'A2:C2');
+data.Properties.VariableNames = names;
+data.Properties.VariableUnits = units;
+
+B = data{:,1};
+spc = data{:,2};
+fitSpc = data{:,3};
+
+plot(B, spc, '-k',  ...
+     B, fitSpc, '--b', 'LineWidth', 1);
+xlim([min(B) max(B)]);
+ylim([min(spc) max(spc)]);
+xlabel(sprintf('%s (%s)', names{1}, units{1}));
+ylabel(sprintf('%s (%s)', names{2}, units{2}));
+tmpName = split(fileName, '.');
+title(tmpName{1}, 'Interpreter','none');
+legend(names{2:end});
+set(gca, 'LineWidth', 1.5);
+set(gcf,'color','w');
+box on
+
+fig = gcf;
+if fig.WindowStyle ~= "docked"
+    set(fig,'position',[10,10,900,600]);
+end
+
+end
