8100eed6c3bad01413daef3386c2c0aa2192e97a
Fanmiao Kong <fanmiaokong1223@gmail.com>
2022-05-11 23:23:50 +0100

    Fix error about timeScaleFactor

--- fit_T1.m
+++ fit_T1.m
@@ -117,6 +117,8 @@ for i = 1:numFiles
     y = real(y);
     % change x axis for T1PF measurement
     x = change_x_axis(x, fullfile(path,f));
+    % Change x axis unit
+    x = x/timeScaleFactor;
     % first use exponfit to get an estimate
     % "c1 + c2*exp(-k*x)"
     [k,c,yfit] = exponfit(x,y,1);
@@ -141,7 +143,7 @@ for i = 1:numFiles
             [curve, gof] = fit(x,y,ft);
             [T_long, T_longPerc, T_short, T_shortPerc] = ...
                 get_Tlong_and_Tshort([curve.k1, curve.k2], ...
-                [curve.c1, curve.c2, curve.c3], timeScaleFactor);
+                [curve.c1, curve.c2, curve.c3]);
             FitResult{i,2:end-1} = [T_long, T_longPerc, T_short, T_shortPerc];
             FitResult.("R-Squared")(i) = gof.rsquare; 
             yfit = curve(x);
@@ -159,8 +161,6 @@ for i = 1:numFiles
             FitResult.("R-Squared")(i) = gof.rsquare; 
             yfit = curve(x);
     end
-    % Change x axis unit
-    x = x/timeScaleFactor;
     % Normalize y axis
     yM = max(y);
     y = y/yM;
