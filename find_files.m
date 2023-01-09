e3c0293e47895906c05da19a1a119f408d0459c6
Fanmiao Kong <fanmiaokong1223@gmail.com>
2023-01-09 17:50:51 +0000

    add documentation

--- find_files.m
+++ find_files.m
@@ -1,19 +1,38 @@
-function outputFiles = find_files(path, keywords, fullPathOutput)
-% Find files in given path based on give keywords. The keywords can be as 
-% many as you want, and can be in any sequence. They keywords list should be
-% a string array.
-arguments
-    path char
-    keywords string % string array
-    fullPathOutput logical = false
-end
+% find_files    Find files in given path based on give keyword
+%   find_files(path, {'keyword1', 'keyword2'})
+%   find_files(path, {'keyword1', 'keyword2'}, 'FileType', 'DTA')
+%   find_files(path, {'keyword1', 'keyword2'}, 'FileType', 'csv')
+%   find_files(path, {'keyword1', 'keyword2'}, 'FullPathOutput', true)
+%
+%   Input:
+%       path        directory to find files
+%       keywords    keywords based on which to find files
+%                   - string: "80K" (if there's only one keyword)
+%                   - character vector: '80K' (if there's only one keyword)
+%                   - string array: ["80K", "3480G"]
+%                   - cell array of character vectors: {'80K', '3480G'}
+%       FileType    optional, can be any type, like .DTA, .csv
+%       FullPathOutput
+%                   optional, if true spit out the absolute path of the
+%                   file(s). If false spit out only the file names
+%
+%   Output:
+%       cell array containing the files
 
-% check what file type to read
-if ~any(contains(keywords, "DTA") | contains(keywords, "DSC"))
-    keywords(end+1) = "DTA"; % by default read DTA file, if no file 
-                                  % type is provided
-end
 
+function outputFiles = find_files(path, keywords, varargin)
+
+par = inputParser;
+par.addParameter('FileType', "DTA", @(x)isstring(x)|ischar(x));
+par.addParameter('FullPathOutput', false, @islogical);
+par.KeepUnmatched = true;
+
+parse(par, varargin{:});
+args = par.Results;
+
+% Add file fype to keywords
+keywords = string(keywords);
+keywords(end+1) = strcat(".", args.FileType); 
 
 % if the temperature keywords is provides, prepend a '_' before searching
 % the file
@@ -29,7 +48,7 @@ indices = cellfun(@(s)contains(allFiles,s), keywords, ...
 finIdx = all(horzcat(indices{:}),2);
 outputFiles = allFiles(finIdx); % one column cell array
 
-if fullPathOutput
+if args.FullPathOutput
     outputFiles = cellfun(@(file)fullfile(path,file), outputFiles, ...
         'UniformOutput',false);
 end
