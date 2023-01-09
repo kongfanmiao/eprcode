e48a8af5cadb25bb5ae370345f08380e489e3a15
Fanmiao Kong <fanmiaokong1223@gmail.com>
2023-01-09 17:49:59 +0000

    move find_parameter out

--- change_x_axis.m
+++ change_x_axis.m
@@ -22,13 +22,3 @@ d30 = find_parameter(DSCfilePath, "d30");
 % the i-th value of xNew = d3+d30*(1+2+...+i-1)+0.5*p9-p2
 xNew = d3+d30*(1:N)'.*(0:N-1)'/2+0.5*p0-p2;
 end
-
-function output = find_parameter(DSCfilePath, param)
-% Find the parameters in DSC file
-fstr = fileread(DSCfilePath); % the whole file string
-% Find the file string that matches the given pattern
-paramStr = regexp(fstr, strcat(param, "\s+=\s+\d+\s+;"), "match");
-paramStr = paramStr{:};
-paramStr = split(paramStr);
-output = str2double(paramStr{end-1});
-end
