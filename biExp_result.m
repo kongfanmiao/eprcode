1fe5ade73648b2ec0cc93d37cb62b132a8dc158d
Fanmiao Kong <fanmiaokong1223@gmail.com>
2023-01-09 17:49:39 +0000

    add xscale argument

--- biExp_result.m
+++ biExp_result.m
@@ -1,4 +1,4 @@
-function biExp_result(FitResult, plot_percentage)
+function biExp_result(FitResult, plot_percentage, xscale)
 
 % Plot the T_long, T_short and their percentages for varying temperatures
 % FitResult: output of fit_T1(fit_Tm) with the bi-exponential fitting model
@@ -6,6 +6,7 @@ function biExp_result(FitResult, plot_percentage)
 arguments
     FitResult   table
     plot_percentage logical = true
+    xscale char = 'linear'
 end
 
 
@@ -15,27 +16,28 @@ end
 
 scatter(FitResult.Temperature, FitResult.T_long, 100, 'blue', 'filled','o');
 hold on
-scatter(FitResult.Temperature, FitResult.T_short, 100,'blue', 'o');
+scatter(FitResult.Temperature, FitResult.T_short, 100, 'blue', 'o');
 ylabel(sprintf("T_{long} & T_{short} (%s)", ...
     FitResult.Properties.VariableUnits{2}));
-
+set(gca, 'ycolor', 'blue')
 legend('T_{long}', 'T_{short}', "Location","best");
 
 if plot_percentage
     yyaxis right
     scatter(FitResult.Temperature, ...
-        FitResult.('T_long Percentage'), 100, 'filled', '^');
+        FitResult.('T_long Weight'), 100, 'filled', '^');
     scatter(FitResult.Temperature, ...
-        FitResult.('T_short Percentage'), 100, 'v');
-    ylabel('Percentage')
+        FitResult.('T_short Weight'), 100, 'v');
+    ylabel('Weight')
     
-    legend('T_{long}', 'T_{short}', 'T_{long} percentage', ...
-        'T_{short} percentage', "Location","best");
+    legend('T_{long}', 'T_{short}', 'T_{long} weight', ...
+        'T_{short} weight', "Location","best");
 end
 
 xlabel('Temperature (K)');
 hold off
 set(gcf,'color','w');
+set(gca, 'xscale', xscale);
 box on
 
 % fig = gcf;
